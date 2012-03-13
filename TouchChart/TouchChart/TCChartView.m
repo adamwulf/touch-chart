6d9f8b57cb674e4cdddf79154324a2209b797bf6
Adam Wulf <adam.wulf@gmail.com>
2012-03-12 23:52:38 -0700

    magic math knows when your finger turns

--- TouchChart/TouchChart/TCChartView.m
+++ TouchChart/TouchChart/TCChartView.m
@@ -28,6 +28,46 @@ CGFloat DistanceBetweenTwoPoints(CGPoint point1,CGPoint point2)
 };
 
 
+//
+// http://stackoverflow.com/questions/1211212/how-to-calculate-an-angle-from-three-points
+CGFloat GetAngle(CGPoint vertex, CGPoint point2, CGPoint point3){
+    
+    // cos-1((P122 + P132 - P232)/(2 * P12 * P13))
+    
+    CGFloat P12 = DistanceBetweenTwoPoints(vertex, point2);
+    CGFloat P13 = DistanceBetweenTwoPoints(vertex, point3);
+    CGFloat P23 = DistanceBetweenTwoPoints(point2, point3);
+
+    
+    CGFloat num = P12 * P12 + P13 * P13 - P23 * P23;
+    CGFloat den = 2 * P12 * P13;
+    CGFloat total = num / den;
+    CGFloat ret = acosf(total);
+    return ret;
+};
+
+
+/*
+;;
+
+Public Function GetAngle(ByVal Ax As Single, ByVal Ay As _
+                         Single, ByVal Bx As Single, ByVal By As Single, ByVal _
+                         Cx As Single, ByVal Cy As Single) As Single
+Dim dot_product As Single
+Dim cross_product As Single
+
+' Get the dot product and cross product.
+dot_product = DotProduct(Ax, Ay, Bx, By, Cx, Cy)
+cross_product = CrossProductLength(Ax, Ay, Bx, By, Cx, _
+                                   Cy)
+
+' Calculate the angle.
+GetAngle = ATan2(cross_product, dot_product)
+End Function 
+
+
+*/
+
 
 
 
@@ -55,7 +95,7 @@ CGFloat DistanceBetweenTwoPoints(CGPoint point1,CGPoint point2)
                 // remove a point
                 [points removeObjectAtIndex:i-1];
                 i--;
-            }else if(DistanceBetweenTwoPoints(p1, p2) < 20){
+            }else if(DistanceBetweenTwoPoints(p1, p2) < 30){
                 [points removeObjectAtIndex:i-1];
                 i--;
             }
@@ -74,6 +114,21 @@ CGFloat DistanceBetweenTwoPoints(CGPoint point1,CGPoint point2)
 
 -(BOOL) isPoint:(CGPoint)point inSlopeOfLine:(NSMutableArray*)lineOfPoints{
     if([lineOfPoints count] < 2) return YES;
+    
+    
+    CGPoint lastValidPoint = [[lineOfPoints lastObject] CGPointValue];
+    CGPoint pointOnFirstLine = [[lineOfPoints objectAtIndex:0] CGPointValue];
+    CGPoint potentialPoint = point;
+    
+
+    CGFloat angle = GetAngle(lastValidPoint, pointOnFirstLine, potentialPoint);
+    
+    NSLog(@"angle between is hopefully near pi, maybe -pi: %f vs %f", angle, angle/M_PI);
+    
+    if(ABS(angle/M_PI) > 0.90){
+        return YES;
+    }
+    return NO;
     
     CGFloat avgSlope = 0;
     BOOL wasMaxFloat = NO;
@@ -179,14 +234,14 @@ CGFloat DistanceBetweenTwoPoints(CGPoint point1,CGPoint point2)
 
 
 -(void) drawRect:(CGRect)rect{
-    CGContextRef context    = UIGraphicsGetCurrentContext();
-    
-    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-    
-    // Draw them with a 2.0 stroke width so they are a bit more visible.
-    CGContextSetLineWidth(context, 2.0);
-    
     if([points count] >= 1 || [pointsInThisLine count] > 1){
+        CGContextRef context    = UIGraphicsGetCurrentContext();
+        
+        CGContextSetLineWidth(context, 1.0);
+        CGContextSetStrokeColorWithColor(context, [UIColor blueColor].CGColor);
+        
+
+        
         NSValue* start = [points objectAtIndex:0];
         CGPoint startP = [start CGPointValue];
         
@@ -205,11 +260,55 @@ CGFloat DistanceBetweenTwoPoints(CGPoint point1,CGPoint point2)
         }
         
         CGContextAddLineToPoint(context, startP.x, startP.y); //draw to this point
-    }
 
-    // and now draw the Path!
-    CGContextStrokePath(context);
+    
+        
+        // and now draw the Path!
+        CGContextStrokePath(context);
+    
+    
+        for(int i=0;i<[points count];i++){
+            [self drawDotAt:[[points objectAtIndex:i] CGPointValue]];
+        }
+        
+    }
 }
+
+-(void) drawDotAt:(CGPoint)currPoint{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(context);
+    // (3) Create the gradient to paint the anchor points.
+    CGFloat colors [] = { 
+        0.4, 0.8, 1.0, 1.0, 
+        0.0, 0.0, 1.0, 1.0
+    };
+    CGColorSpaceRef baseSpace = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(baseSpace, colors, NULL, 2);
+    CGColorSpaceRelease(baseSpace), baseSpace = NULL;
+    
+    // (4) Set up the stroke for drawing the border of each of the anchor points.
+    CGContextSetLineWidth(context, 1);
+    CGContextSetShadow(context, CGSizeMake(0.5, 0.5), 1);
+    CGContextSetStrokeColorWithColor(context, [UIColor whiteColor].CGColor);
+    
+    CGRect currRect = CGRectMake(currPoint.x-5, currPoint.y-5, 10, 10);
+    
+    // (5) Fill each anchor point using the gradient, then stroke the border.
+    CGContextSaveGState(context);
+    CGContextAddEllipseInRect(context, currRect);
+    CGContextClip(context);
+    CGPoint startPoint = CGPointMake(CGRectGetMidX(currRect), CGRectGetMinY(currRect));
+    CGPoint endPoint = CGPointMake(CGRectGetMidX(currRect), CGRectGetMaxY(currRect));
+    CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, 0);
+    CGContextRestoreGState(context);
+    CGContextStrokeEllipseInRect(context, CGRectInset(currRect, 1, 1));
+
+    CGGradientRelease(gradient), gradient = NULL;
+    CGContextRestoreGState(context);
+
+}
+
+
 
 
 
