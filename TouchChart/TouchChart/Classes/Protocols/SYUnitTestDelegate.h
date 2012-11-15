ff0de46f24daf14393113e05060b6275f1a4cfe8
Fernando Torcelly <fgtorcelly@sylion.com>
2012-11-15 19:06:11 +0000

    Cleaning Code
    - Updated: First version clean code.
    - Need: Check some comments.
    
    Signed-off-by: Fernando Torcelly <fgtorcelly@sylion.com>

---
+++ TouchChart/TouchChart/Classes/Protocols/SYUnitTestDelegate.h
@@ -0,0 +1,13 @@
+//
+//  SYUnitTestDelegate.h
+//  TouchChart
+//
+//  Created by Fernando Garcia Torcelly on 13/11/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SYUnitTestDelegate <NSObject>
+
+@end
