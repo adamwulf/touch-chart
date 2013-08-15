fdfa967038567fc670834cf96c204c78dc3a435a
Adam Wulf <adam.wulf@gmail.com>
2013-08-15 16:26:25 -0500

    added framework header file

---
+++ TouchChart/TouchChart/TouchShape.h
@@ -0,0 +1,18 @@
+//
+//  TouchShape.h
+//  TouchChart
+//
+//  Created by Adam Wulf on 8/15/13.
+//
+//
+
+#import <TouchShape/SYPaintView.h>
+#import <TouchShape/SYPaintViewDelegate.h>
+#import <TouchShape/SYVectorView.h>
+#import <TouchShape/SYShape.h>
+#import <TouchShape/SYGeometry.h>
+#import <TouchShape/SYSegment.h>
+#import <TouchShape/SYBezier.h>
+#import <TouchShape/SYBezierController.h>
+#import <TouchShape/TCShapeController.h>
+
